0dd783d19fe2b42479b98255aa64e87ca561e6cb
kyohei uno <waizeru00@gmail.com>
2022-12-09 05:45:23 +0900

    UITableViewにダミーデータを表示その2

--- tweet_app/HomeViewController.swift
+++ tweet_app/HomeViewController.swift
@@ -11,22 +11,29 @@ import UIKit
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    //配列でダミーデータを設定する
-    let fakeDate = ["ダミーデータその1", "ダミーデータその2", "ダミーデータその3", "ダミーデータその4", "ダミーデータその5", "ダミーデータその6", "ダミーデータその7ダミーデータその7ダミーデータその7ダミーデータその7ダミーデータその7ダミーデータその7"]
+    //ダミーデータを定義
+    
+    let fakeData = ["ダミーデータその１","ダミーデータその2","ダミーデータその3","ダミーデータその4","ダミーデータその5ダミーデータその5ダミーデータその5",]
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         tableView.dataSource = self
     }
 }
 
+////tableViewの拡張
 extension HomeViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fakeDate.count //この箇所に全てのツイート数を変数にして返す・現時点ではダミーデータの個数を返す
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fakeData.count //この箇所に全てのツイート数を変数にして返す・現時点ではダミーデータの個数を返す
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        cell.textLabel!.text = fakeDate[indexPath.row]
+        cell.textLabel?.text = fakeData[indexPath.row]
         cell.textLabel!.numberOfLines = 0 //0に設定
         return cell
     }
