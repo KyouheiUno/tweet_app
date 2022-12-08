a3a56cb9180e89a6dcb2ebd93d230079fbce62c5
kyohei uno <waizeru00@gmail.com>
2022-12-09 01:09:45 +0900

    HomeViewController作成

---
+++ tweet_app/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  tweet_app
+//
+//  Created by 宇野恭平 on R 4/12/09.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        print("HomeViewControllerが表示されました。")
+    }
+}
