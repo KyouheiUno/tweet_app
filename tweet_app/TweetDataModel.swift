77bee92c92f35df14d1db4f0450aae46e2142702
kyohei uno <waizeru00@gmail.com>
2022-12-31 08:03:31 +0900

    Tweetモデルを定義

---
+++ tweet_app/TweetDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  TweetDataModel.swift
+//  tweet_app
+//
+//  Created by 宇野恭平 on R 4/12/31.
+//
+
+import Foundation
+import RealmSwift
+
+class MemoDataModel: Object {
+    @objc dynamic var id: String = UUID().uuidString // データを一意に識別するための識別子
+    @objc dynamic var userName: String = ""
+    @objc dynamic var tweetText: String = ""
+    @objc dynamic var recordDate: Date = Date()
+}
