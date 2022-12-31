450615d751e9cc13cd339fcac61d66ec1939b317
kyohei uno <waizeru00@gmail.com>
2022-12-31 09:23:05 +0900

    ツイート内容の保存処理の実装

--- tweet_app/HomeViewController.swift
+++ tweet_app/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -15,6 +16,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var addTweetButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     
+    var tweetDataList: [TweetDataModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,26 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         //xibファイルを登録する
         tableView.register(UINib(nibName: "TweetViewCell", bundle: nil), forCellReuseIdentifier: "customTweetCell")
         configureTableView()
-        addConfigureTweetButton() //新規ツイートボタンの
+        setTweetData()
+        addConfigureTweetButton()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setTweetData()
+        tableView.reloadData()
+    }
+    
+    //キーボード以外の箇所を押下でキーボードを閉じる処理
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    //ツイートモデルのデータの表示
+    func setTweetData() {
+        let realm = try! Realm()
+        let tweetData = realm.objects(TweetDataModel.self).sorted(byKeyPath: "recordDate", ascending: false)
+        tweetDataList = Array(tweetData)
     }
     
     //新規ツイート画面への遷移ボタンの構成
@@ -43,14 +64,15 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return tweetDataList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customTweetCell", for: indexPath) as! TweetViewCell
         cell.userIcon.image = UIImage(systemName: "swift")
-        cell.tweetText.text = "これはサンプルのテキストです。これはサンプルのテキストです。これはサンプルのテキストです。これはサンプルのテキストです。これはサンプルのテキストです。"
-        cell.userName.text = "サンプルユーザー名"
+        let tweetDataModel: TweetDataModel = tweetDataList[indexPath.row]
+        cell.userName.text = tweetDataModel.userName
+        cell.tweetText.text = tweetDataModel.tweetText
         return cell
     }
 }
